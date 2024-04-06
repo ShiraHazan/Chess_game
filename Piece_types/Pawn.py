4e20f955ee61e91c1371d33b30a92b1af5c9cffd
Shira Hazan <shira27813@gmail.com>
2024-04-07 01:36:59 +0300

    Add a 'has_moved' attribute to the 'Pawn' class

--- Piece_types/Pawn.py
+++ Piece_types/Pawn.py
@@ -5,6 +5,7 @@ from Enums.PieceType import PieceType
 class Pawn(Piece):
     def __init__(self, color):
         super().__init__(color, PieceType.PAWN)
+        self.has_moved = False  # Flag to track if the pawn has moved
 
     def check_move(self, start_pos, end_pos):
         # Code to check if a move is valid for a pawn
