2ef466cbeb5e0f5ac447a95d455a7d8f1adef8ef
Shira Hazan <shira27813@gmail.com>
2024-04-07 01:23:01 +0300

    Change the '__init__' function of 'Queen' to use 'PieceType' enum

--- Piece_types/Queen.py
+++ Piece_types/Queen.py
@@ -1,10 +1,10 @@
 from Piece import Piece
+from Enums.PieceType import PieceType
 
 
 class Queen(Piece):
     def __init__(self, color):
-        super().__init__(color)
-        self.type = "Queen"
+        super().__init__(color, PieceType.QUEEN)
 
     def check_move(self, start_pos, end_pos):
         """Code to check if a move is valid for a queen"""
