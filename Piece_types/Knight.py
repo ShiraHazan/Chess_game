b97d1a9a51f5d4334f24be65cccbe9e62d5970c7
Shira Hazan <shira27813@gmail.com>
2024-04-07 01:20:50 +0300

    Change the '__init__' function of 'Knight' to use 'PieceType' enum

--- Piece_types/Knight.py
+++ Piece_types/Knight.py
@@ -1,10 +1,10 @@
 from Piece import Piece
+from Enums.PieceType import PieceType
 
 
 class Knight(Piece):
     def __init__(self, color):
-        super().__init__(color)
-        self.type = "Knight"
+        super().__init__(color, PieceType.KNIGHT)
 
     def check_move(self, start_pos, end_pos):
         """Code to check if a move is valid for a knight"""
